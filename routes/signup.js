9e9154158cfc1cd45793563d492ba76348da22ca
ho-yi-shiuan <hys10130857@gmail.com>
2020-02-05 14:12:01 +0800

    add function: signup(local)

---
+++ routes/signup.js
@@ -0,0 +1,92 @@
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+var mysql = require("../mysqlcon.js");
+var express = require("express");
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/', async function(req, res){
+	console.log("進到signup api !");
+	//檢查是否已註冊
+	var email = req.body.email;
+	var check_email = "SELECT * FROM user WHERE email = \"" + req.body.email + "\";";
+	const check_promise = new Promise((resolve, reject) => {
+		mysql.con.query(check_email,function(err, result){
+			if(result.length == 0){
+				//無人註冊
+				resolve(1);
+			}else{
+				//有人註冊
+				reject("此帳號已註冊過");
+				res.end("3");
+			}
+		});
+	})
+	let flag = await check_promise;
+	if(flag){
+		//開始註冊
+		//製造token
+		var date = Date.now();
+		function createtoken(email){
+			let hash = crypto.createHash('sha256');
+			hash.update(email);
+			const token = hash.digest('hex');
+			return token;
+		}
+		token = createtoken(req.body.email+date);
+		var time = 3600;
+		var expire = parseInt(date+time);
+		//加密password
+		const saltRounds = 10;
+		const bcrypt_promise = new Promise((resolve, reject) => {
+			const hash = bcrypt.hashSync(req.body.password, saltRounds);
+			resolve(hash);
+		})
+		let bcrypt_password = await bcrypt_promise;
+		var signup_data = {
+			provider: "native",
+			email: req.body.email,
+			password: bcrypt_password,
+			name: req.body.name,
+			access_token: token,
+			access_expired: expire
+		}
+		console.log(signup_data);
+		const insert_signup_promise = new Promise((resolve, reject) => {
+			mysql.con.query("INSERT INTO user SET?", signup_data, function(err, result){
+				if(err){
+					console.log("signup api: ");
+					console.log(err);
+				}
+				else
+				{				
+					console.log("signup api: 新增user成功");
+					resolve(result);
+				}
+			});
+		})
+		let insert_signup_result = await insert_signup_promise;
+		var user_information = {
+			id: insert_signup_result.insertId,
+			provider: signup_data.provider,
+			email: signup_data.email,			
+			name: signup_data.name,
+			picture: null
+		};
+		var data = {
+			access_token: token,
+			access_expired: expire,
+			user: user_information
+		};
+		var list = {
+			data: data
+		};
+		console.log(list);
+		res.cookie("user",token);
+		res.send(list);
+	}
+	});
+
+module.exports = app;
--- socket.js
+++ socket.js
@@ -2,51 +2,64 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const AWS = require('aws-sdk');
+var mysql = require("./mysqlcon.js");
 var bodyParser = require('body-parser');
 var mysql = require("mysql");
 
-const con = mysql.createPool({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'hys10130857',
-  database: "person_project"
-})//連接到mySQL
+AWS.config.loadFromPath('./s3_config.json');
+var s3Bucket = new AWS.S3( { params: {Bucket: 'yssites.com/person_project/socket'} } );
 
-app.use(express.static("public"));// 待改善: 訊息按enter可直接發送
-//新增走失紀錄api
+app.use(express.static("public"));// 待改善: 訊息按 enter可直接發送
+//新增走失紀錄 api
 app.use('/lost_record', require('./routes/lost_record'));
+//room 裡面用到的api
+app.use('/lost_detail', require('./routes/lost_detail'));
+app.use('/lost_mark', require('./routes/lost_mark'));
+//會員系統api
+app.use('/signup', require('./routes/signup'));
+
+//各房間的user list
+var roomInfo = {};
 
 // 當發生連線事件
 io.on('connection', async function(socket){
-    console.log("user connected");
-	console.log(socket.request.headers.referer);
+	//拆解網址找出id
+	var url = socket.request.headers.referer;
+	var splited = url.split('=');
+	var room_id = splited[splited.length - 1];
+	//加入房間
+	socket.join(room_id);
+	console.log('有人加入了room ' + room_id);
 	//歷史訊息
 	const messsage_promise = new Promise((resolve, reject) => {
-		con.query("SELECT name, content, time from socket", function(err,res){
+		con.query("SELECT name, content, content_type, time from socket"+room_id, function(err,res){
 			if(err){
 				console.log(err);
 			}else{
-				console.log("select history message successful!");
+				console.log("select history message of room "+room_id+" successful!");
 				resolve(res);
 			}
 		})
 	})
 	const history = await messsage_promise;
+	console.log("後端送出歷史訊息如下:");
+	console.log(history);
 	const socketid = socket.id;
 	io.to(socketid).emit('history', history);
 	
 	//接收前端message事件
-	socket.on("message", function(obj){
+	socket.on("message_to_backend", function(obj){
 		console.log("收到前端: ");
 		console.log(obj);
 		//把資料新增進db
-		var data = {
+		var data_text = {
 			name:obj.name,
 			content:obj.content,
+			content_type: "text",
 			time:Date.now()
 		}
-		con.query("INSERT INTO socket set?", data, function(err,res){
+		con.query("INSERT INTO socket" +room_id+ " set?", data_text, function(err,res){
 			if(err){
 				console.log(err);
 			}else{
@@ -55,13 +68,62 @@ io.on('connection', async function(socket){
 		})
 		
 		//回傳給前端
-		io.emit("message", obj);
+		io.to(room_id).emit("message", data_text);
 	});
+	//前端上傳圖片給後端
+	socket.on('sendImg', (obj) => {
+		console.log("socket收到前端圖片");
+		var data = {
+			name:obj.name,
+			content:"",
+			type_of_content: "image",
+			time:Date.now()
+		}
+		//s3 code start
+		const base64Data = new Buffer.from(obj.img.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+		const type = obj.img.split(';')[0].split('/')[1];
+		const params = {
+			Key: Date.now()+obj.name+"."+type, // type is not required
+			Body: base64Data,
+			ContentEncoding: 'base64', // required
+			ContentType: "image/"+type // required. Notice the back ticks
+		  }
+		const promise1 = new Promise(function(resolve, reject) {
+			s3Bucket.upload(params, function(err, upload_data){
+				if(err){ 
+					console.log(err);
+					console.log('Error uploading data: ', upload_data); 
+				}else{
+					console.log('succesfully uploaded the image!');
+					console.log("Key: "+upload_data.Key);
+					resolve(upload_data.Key);
+				}
+			});	
+		});
+		promise1.then(function(key) {
+			data.content = key;
+			con.query("INSERT INTO socket" +room_id+ " set?", data, function(err,res){
+				if(err){
+					console.log(err);
+				}else{
+					console.log("insert message(picture) to db successful!");
+				}
+			})
+			io.to(room_id).emit('receiveImg', data);
+		});
+		//s3 code end
+	});
+	// 當發生離線事件
+	socket.on("disconnect", function(){
+		console.log('有人離開了room ' + room_id);
+		socket.leave(room_id);
+	});
+});
 
-    // 當發生離線事件
-    socket.on("disconnect", function(){
-        console.log("user disconnected");
-    });
+
+app.get('/room', async function(req, res){
+	socketid = req.query.id;
+	res.sendFile(__dirname + '/public/socket.html');
 });
 
 // 注意，這邊的 server 原本是 app 
