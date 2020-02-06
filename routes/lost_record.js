171e7a96b99a6057a09bc60766dcbd4bd0d127f7
ho-yi-shiuan <hys10130857@gmail.com>
2020-02-06 22:57:14 +0800

    modified css and added UI design of login/ signup page

--- routes/lost_record.js
+++ routes/lost_record.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const AWS = require('aws-sdk');
-var mysql = require("mysql");
+var mysql = require("../mysqlcon.js");
 var express = require("express");
 var multer  = require('multer');
 var multerS3 = require('multer-s3');
@@ -8,10 +8,8 @@ var bodyparser = require('body-parser');
 var app = express();
 app.use('/public',express.static('public'));
 
-const s3 = new AWS.S3({
-    accessKeyId: "AKIAI4YXVKNLMJDWWJTQ",
-    secretAccessKey: "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"
-});
+AWS.config.loadFromPath('./s3_config.json');
+const s3 = new AWS.S3();
 
 var upload = multer({
   storage: multerS3({
@@ -24,17 +22,13 @@ var upload = multer({
   })
 })
 
-const con = mysql.createPool({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'hys10130857',
-  database: "person_project"
-})//連接到mySQL
-
-app.post('/', upload.single('image'), function(req, res){
-	console.log("lost_record api: "+req.file.key);
+app.post('/', upload.single('image'), async function(req, res){
+	console.log(req.body);
+	//待辦:
+	//是否要做transaction?
+	var lost_data_id;
 	var lost_data = {
+		category: req.body.category,
 		name: req.body.pet_name,
 		picture: req.file.key,
 		gender: req.body.pet_gender,
@@ -43,26 +37,44 @@ app.post('/', upload.single('image'), function(req, res){
 		color: req.body.pet_color,
 		lost_location: req.body.lost_address,
 		lost_time: req.body.lost_time,
-		other: req.body.other
+		other: req.body.other,
+		user_id: req.body.user_id
 	}
-	con.query("INSERT INTO lost_pet set?",lost_data,function(err, result){
+	const insert_lost_pet_promise = new Promise((resolve, reject) => {
+		mysql.con.query("INSERT INTO lost_pet set?",lost_data,function(err, result){
+			if(err){
+				console.log("lost_record api(post): \n");
+				console.log(err);
+			}
+			else
+			{
+				resolve(result);
+				lost_data_id = result.isertId;
+				console.log("lost_record api: db新增走失紀錄成功");
+			}
+		});
+	})
+	const insert_lost_pet = await insert_lost_pet_promise;
+	var create_chat_table = "CREATE TABLE socket"+insert_lost_pet.insertId+"(id bigint(20) NOT NULL AUTO_INCREMENT, name varchar(45) DEFAULT NULL, content varchar(45) DEFAULT NULL, content_type varchar(20), time bigint(20) DEFAULT NULL, PRIMARY KEY (id));";
+	mysql.con.query(create_chat_table,function(err, result){
 		if(err){
-			console.log("lost_record api(post): \n");
+			console.log("lost_record api(post, create chat table): \n");
 			console.log(err);
 		}
 		else
 		{
-			console.log("lost_record api: db新增走失紀錄成功");
+			console.log("lost_record api: db新增聊天室table成功");
+			res.send("新增走失紀錄成功!");
 		}
-	});
-
+	});	
+	
 });
 
 app.get('/', async function(req, res){
 	var picture_s3_url = "https://d2h10qrqll8k7g.cloudfront.net/person_project/lost_pet/";
 	var select_lost_record = "SELECT * from lost_pet;"
 	const lost_record_promise = new Promise((resolve, reject) => {
-		con.query(select_lost_record, function(err, result){
+		mysql.con.query(select_lost_record, function(err, result){
 			if(err){
 				console.log("lost_record api(get): \n");
 				console.log(err);
@@ -75,6 +87,7 @@ app.get('/', async function(req, res){
 	var lost_record_array = [];
 	for(i=0; i<lost_record.length; i++){
 		var lost_data_object = {
+			id: lost_record[i].id,
 			name: lost_record[i].name,
 			picture: picture_s3_url+lost_record[i].picture,
 			gender: lost_record[i].gender,
