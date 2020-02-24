d5176720b479110e3a2c2f31a943eeec3d43b695
ho-yi-shiuan <hys10130857@gmail.com>
2020-02-24 23:52:42 +0800

    nodified UI also search by image function

---
+++ public/js/index.js
@@ -0,0 +1,444 @@
+function row(){
+	$('html, body').animate({
+		scrollTop: $("#category").offset().top
+	}, 500);
+}
+<!-- Initialize the plugin: -->
+$(document).ready(function() {
+	$('[name="select_breed"]').multiselect();
+	$('[name="select_color"]').multiselect();
+});
+//google map code start
+console.log("開始執行map script");
+function initMap(map_id,locate) {
+	console.log("執行init map");
+	var geocoder = new google.maps.Geocoder();
+	var map = new google.maps.Map(document.getElementById(map_id), {
+		zoom: 12,
+		styles:[
+		  {
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#f5f5f5"
+			  }
+			]
+		  },
+		  {
+			"elementType": "labels.icon",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#616161"
+			  }
+			]
+		  },
+		  {
+			"elementType": "labels.text.stroke",
+			"stylers": [
+			  {
+				"color": "#f5f5f5"
+			  }
+			]
+		  },
+		  {
+			"featureType": "administrative.land_parcel",
+			"elementType": "labels",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"featureType": "administrative.land_parcel",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#bdbdbd"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#eeeeee"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi",
+			"elementType": "labels.text",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#757575"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi.business",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi.park",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#e5e5e5"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi.park",
+			"elementType": "labels.text",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi.park",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#9e9e9e"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#ffffff"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.arterial",
+			"elementType": "labels",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.arterial",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#757575"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.highway",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#dadada"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.highway",
+			"elementType": "labels",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.highway",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#616161"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.local",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.local",
+			"elementType": "labels",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.local",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#9e9e9e"
+			  }
+			]
+		  },
+		  {
+			"featureType": "transit.line",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#e5e5e5"
+			  }
+			]
+		  },
+		  {
+			"featureType": "transit.station",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#eeeeee"
+			  }
+			]
+		  },
+		  {
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#c9c9c9"
+			  }
+			]
+		  },
+		  {
+			"featureType": "water",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#9e9e9e"
+			  }
+			]
+		  }
+		],
+		mapTypeControl: false,
+		fullscreenControl: false,
+		rotateControl: false,
+		scaleControl: false,
+		streetViewControl: false,
+		zoomControl: false,
+		center: locate
+	});
+	let marker = new google.maps.Marker({
+		map: map,
+		position: locate,
+		icon: '/images/black_pin.png'
+	});
+};
+//googlt map code end
+//地址搜尋框
+var autocomplete;
+window.addEventListener('load', () => {
+	var input = document.getElementsByName('input_address')[0];
+	autocomplete = new google.maps.places.Autocomplete(input);
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		fillInAddress();
+	});		
+});
+function fillInAddress(){
+	var place = autocomplete.getPlace();
+	document.getElementsByName("lost_address_lng")[0].value = place.geometry.location.lng();
+	document.getElementsByName("lost_address_lat")[0].value = place.geometry.location.lat();
+};
+//googlt map code end
+//ajax 送出表單 code start
+document.getElementById("submit").addEventListener('click', function(){	
+	if(document.getElementsByName("input_address")[0].value.length > 0){
+		if(document.getElementsByName("lost_address_lng")[0].value.length == 0){
+			//輸入地址且地址錯誤
+			document.getElementById("error_message").style = "color: red; display: block";
+		}else{
+			ajax_search();
+			document.getElementById("error_message").style = "display: none";
+		}
+	}else{
+		ajax_search();
+		document.getElementById("error_message").style = "display: none";
+	}
+});
+//ajax 送出表單 code start*/
+window.addEventListener('load', () => {
+	const xhr = new XMLHttpRequest();
+	var url = "/lost_record?lost_status=finding";
+	xhr.open("get", url);
+	xhr.onload = function(){
+		var pets = JSON.parse(this.response);
+		if(pets.data.length == 0){
+			//顯示目前沒有協尋中寵物
+		}else{
+			console.log(pets);
+			append_data(pets.data);
+		}
+	}
+	xhr.send();
+});
+$("#image").on("change",function (e) {
+	console.log("已選檔案");
+	var e = e || window.event;
+	//獲取 檔案 個數 取消的時候使用
+	var files = e.target.files;
+	console.log(files[0].name);
+	if(files.length>0){
+		// 獲取檔名 並顯示檔名
+		var fileName = files[0].name;
+		document.getElementById("chose_picture").innerHTML = fileName;
+	}else{
+		//清空檔名
+		document.getElementById("chose_picture").innerHTML = "以照片搜尋";
+	}
+});	
+function ajax_search(){
+	var form = $('form')[0];
+	var formData = new FormData(form);
+	console.log(formData);
+	$.ajax({
+		contentType: false,
+		cache: false,
+		processData: false,
+		type: "post",
+		url: "/search",
+		data: formData,
+		success: function(res){
+			document.getElementById("image").outerHTML = document.getElementById("image").outerHTML;
+			document.getElementById("chose_picture").innerHTML = "以照片搜尋";
+			console.log(res);
+			document.getElementsByClassName("post_container")[0].innerHTML = "";
+			if(res.image_compare == "no_matched"){
+				//圖片沒有比對到
+				document.getElementById("image_result").innerHTML = "無符合圖片, 以下為文字篩選結果: ";
+			}else{
+				document.getElementById("image_result").innerHTML = "";
+			}
+			append_data(res.data);
+		},
+		error: function(err){
+			console.log(err);
+		}
+	});	
+}
+function append_data(data){
+	for(i=data.length-1; i>=0; i--){
+		let link = document.createElement("a");
+		link.className = "post_link";
+		link.href = "/room?id="+data[i].id;
+		let post = document.createElement("div");
+		post.className = "post";
+		let post_img_container = document.createElement("div");
+		post_img_container.className = "post_img_container";
+		let post_image = document.createElement("img");
+		post_image.className = "post_image";
+		post_image.src = data[i].picture;
+		post_img_container.appendChild(post_image);
+		
+		let post_content_container = document.createElement("div");
+		post_content_container.className = "post_content_container";
+		let post_tag = document.createElement("div");
+		post_tag.id = "post_tag";
+		if(data[i].post_type == "lost"){
+			post_tag.appendChild(document.createTextNode("走失協尋"));
+		}else{
+			post_tag.appendChild(document.createTextNode("拾獲刊登"));
+		}
+		post_content_container.appendChild(post_tag);
+		let post_content = document.createElement("div");
+		post_content.className = "post_content";
+		let post_content_title = document.createElement("div");
+		post_content_title.id = "post_content_title";
+		post_content_title.appendChild(document.createTextNode(data[i].title));
+		post_content.appendChild(post_content_title);
+		post_content_container.appendChild(post_content);
+		let post_flag_container = document.createElement("div");
+		post_flag_container.className = "post_flag_container";
+		if(data[i].gender){
+			let post_flag_gender = document.createElement("div");
+			post_flag_gender.className = "post_flag";
+			if(data[i].gender == "male"){
+				post_flag_gender.appendChild(document.createTextNode("男"));			
+			}else{
+				post_flag_gender.appendChild(document.createTextNode("女"));			
+			}
+			post_flag_container.appendChild(post_flag_gender);			
+		}			
+		if(data[i].age){
+			let post_flag_age = document.createElement("div");
+			post_flag_age.className = "post_flag";
+			post_flag_age.appendChild(document.createTextNode(data[i].age));
+			post_flag_container.appendChild(post_flag_age);			
+		}
+		if(data[i].breed){
+			let post_flag_breed = document.createElement("div");
+			post_flag_breed.className = "post_flag";
+			post_flag_breed.appendChild(document.createTextNode(data[i].breed));
+			post_flag_container.appendChild(post_flag_breed);			
+		}			
+		post_content.appendChild(post_flag_container);
+		let post_content_time = document.createElement("div");
+		post_content_time.id = "post_content_time";
+		post_content_time.appendChild(document.createTextNode("走失時間: "+data[i].lost_time));
+		post_content.appendChild(post_content_time);
+		let post_content_location = document.createElement("div");
+		post_content_location.id = "post_content_location";
+		post_content_location.appendChild(document.createTextNode("走失地點: "+data[i].lost_location));
+		post_content.appendChild(post_content_location);			
+		let post_content_text = document.createElement("div");
+		post_content_text.id = "post_content_text";
+		let content = document.createElement("p");
+		content.className = "content";
+		content.appendChild(document.createTextNode(data[i].content));
+		post_content_text.appendChild(content);
+		post_content.appendChild(post_content_text);
+		let post_button_container = document.createElement("div");
+		post_button_container.className = "post_button_container";
+		let index_map = document.createElement("div");
+		index_map.className = "index_map";
+		index_map.id = "map"+i;
+		post_button_container.appendChild(index_map);
+		post.appendChild(post_img_container);
+		post.appendChild(post_content_container);
+		post.appendChild(post_button_container);
+		link.appendChild(post);
+		document.getElementsByClassName("post_container")[0].appendChild(link);
+		let map_id = "map"+i;
+		let locate = {
+			lat: data[i].lost_location_lat, // 經度
+			lng: data[i].lost_location_lng // 緯度
+		};
+		console.log(map_id);
+		console.log(locate);
+		initMap(map_id,locate);
+	} 
+}
---
+++ routes/search.js
@@ -0,0 +1,293 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+var mysql = require("../mysqlcon.js");
+var express = require("express");
+var multer  = require('multer');
+var multerS3 = require('multer-s3');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/public',express.static('public'));
+
+AWS.config.loadFromPath('./s3_config.json');
+const s3 = new AWS.S3();
+
+var upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'shiuan/person_project/lost_pet',
+	contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (req, file, cb) {
+      cb(null, Date.now()+file.originalname);// 檔案命名要重新想!!!!是否綁room id?? 這樣前端比較好叫
+    }
+  })
+})
+
+app.post('/', upload.single('image'), async function(req, res){
+	console.log(req.file);
+	//篩選類別, 品種, 性別
+	var select_array = [];
+	var body_array = [];
+	var condition_array = [];
+	if(typeof(req.body.lost_status) == "object"){
+		let lost_status_body = " lost_status in (?)";
+		condition_array.push(req.body.lost_status);
+		select_array.push(lost_status_body);
+	}else if(typeof(req.body.lost_status) == "string"){
+		let lost_status_body = " lost_status in (?)";
+		condition_array.push([req.body.lost_status]);
+		select_array.push(lost_status_body);
+	}
+	if(typeof(req.body.post_type) == "object"){
+		let post_type_body = " post_type in (?)";
+		condition_array.push(req.body.post_type);
+		select_array.push(post_type_body);
+	}else if(typeof(req.body.post_type) == "string"){
+		let post_type_body = " post_type in (?)";
+		condition_array.push([req.body.post_type]);
+		select_array.push(post_type_body);
+	}
+	if(typeof(req.body.select_category) == "object"){
+		let category_body = " category in (?)";
+		condition_array.push(req.body.select_category);
+		select_array.push(category_body);
+	}else if(typeof(req.body.select_category) == "string"){
+		let category_body = " category in (?)";
+		condition_array.push([req.body.select_category]);
+		select_array.push(category_body);
+	}
+	if(typeof(req.body.select_breed) == "object"){
+		let breed_body = " breed in (?)";
+		condition_array.push(req.body.select_breed);
+		select_array.push(breed_body);
+	}else if(typeof(req.body.select_breed) == "string"){
+		let breed_body = " breed in (?)";
+		condition_array.push([req.body.select_breed]);
+		select_array.push(breed_body);
+	}
+	if(typeof(req.body.select_gender) == "object"){
+		let gender_body = " (gender is null or gender in (?))";
+		condition_array.push(req.body.select_gender);
+		select_array.push(gender_body);
+	}else if(typeof(req.body.select_gender) == "string"){
+		let gender_body = " (gender is null or gender in (?))";
+		condition_array.push([req.body.select_gender]);
+		select_array.push(gender_body);
+	}
+	//地址篩經緯度
+	if(req.body.lost_address_lng){
+		select_array.push(" lost_location_lng BETWEEN "+req.body.lost_address_lng+"-0.05 AND "+req.body.lost_address_lng+"+0.05 AND lost_location_lat BETWEEN "+req.body.lost_address_lat+"-0.05 AND "+req.body.lost_address_lat+"+0.05");
+	}
+	//顏色where color like '%黑%' OR '%白%'
+	if(typeof(req.body.select_color) == "object"){
+		var color_body = "";
+		color_body += " (";
+		var color_body;
+		for(let j=0; j<req.body.select_color.length; j++){
+			color_body += "color LIKE '%"+req.body.select_color[j]+"%'";
+			if(j <req.body.select_color.length-1){
+				color_body += " OR ";
+			}
+		}
+		color_body += ")";
+		select_array.push(color_body);
+		console.log(color_body);
+	}else if(typeof(req.body.select_color) == "string"){
+		select_array.push(" color LIKE '%"+req.body.select_color+"%'");
+	}
+	
+	var select_body = "SELECT * from lost_pet ";
+	if(select_array.length >0){
+		select_body += "where";
+		for(let i=0; i<select_array.length; i++){
+			select_body += select_array[i];
+			if(i < select_array.length-1){
+				select_body += " AND";
+			}
+		}
+	}
+	select_body += "AND lost_status = \"finding\";";
+	console.log(select_body);
+	console.log(condition_array);
+	const lost_record_promise = new Promise((resolve, reject) => {
+	mysql.con.query(select_body, condition_array, function(err, result){
+			if(err){
+				console.log("lost_record api(get): \n");
+				console.log(err);
+			}else{
+				resolve(result);
+			}
+		});
+	})
+	let lost_record = await lost_record_promise;
+	//以上為純文字篩選結果, 要抓他們的圖片名稱去比對
+	if(req.file){
+		var matched_array = [];
+		console.log("search with file");
+		let image_compare_promise = new Promise((resolve, reject) => {
+			console.log(req.file.key);
+			const select_bucket = 'shiuan';
+			const client = new AWS.Rekognition();
+			const photo_target  = 'person_project/lost_pet/'+req.file.key;
+			console.log("target: "+photo_target);
+			for(let j=0; j<lost_record.length; j++){
+				const photo_source  = 'person_project/lost_pet/'+lost_record[j].pet_picture;
+				console.log("source: "+photo_source);
+				const params = {
+					SourceImage: {
+						S3Object: {
+							Bucket: select_bucket,
+							Name: photo_source
+						},
+					},
+					TargetImage: {
+						S3Object: {
+							Bucket: select_bucket,
+							Name: photo_target
+						},
+					},
+					SimilarityThreshold: 70
+				}
+				client.compareFaces(params, function(err, response) {
+					console.log("開始比對");
+					if (err) {
+						console.log(j+" : error, can't compare");
+						console.log(err);
+						lost_record[j].image_compare_result = "unmatch";
+						if(j == lost_record.length-1){
+							resolve(j+" : finished");
+						}
+					} else {
+						if(response.FaceMatches.length == 0){
+							//無error但沒有比對到
+							console.log(j+" : no matched data");
+							lost_record[j].image_compare_result = "unmatch";
+							if(j == lost_record.length-1){
+								resolve(j+" : finished");
+							}
+						}else{
+							//無error有比對到
+							response.FaceMatches.forEach(data => {
+								let position   = data.Face.BoundingBox
+								let similarity = data.Similarity
+								matched_array.push(j);
+								console.log(j+" : matched");
+								console.log(`The face at: ${position.Left}, ${position.Top} matches with ${similarity} % confidence`);
+								lost_record[j].image_compare_result = "match";
+								if(j == lost_record.length-1){
+									resolve(j+" : finished");
+								}								
+							})
+						}
+					}
+				});		
+			}
+		})
+		image_compare_promise.then(function(){
+			console.log(matched_array);
+			if(matched_array.length > 0 ){
+				var picture_s3_url = "https://d2h10qrqll8k7g.cloudfront.net/person_project/lost_pet/";
+				var matched_lost_record_array = [];
+				for(let i=0; i<lost_record.length; i++){
+					if(lost_record[i].image_compare_result == "match"){
+						var lost_data_object = {
+							id: lost_record[i].pet_id,
+							name: lost_record[i].pet_name,
+							picture: picture_s3_url+lost_record[i].pet_picture,
+							gender: lost_record[i].gender,
+							age: lost_record[i].age,
+							breed: lost_record[i].breed,
+							color: lost_record[i].color,
+							lost_location: lost_record[i].lost_location,
+							lost_location_lng: lost_record[i].lost_location_lng,
+							lost_location_lat: lost_record[i].lost_location_lat,
+							lost_time: lost_record[i].lost_time,
+							lost_status: lost_record[i].lost_status,
+							post_type: lost_record[i].post_type,
+							title: lost_record[i].title,
+							content: lost_record[i].content,
+							image_compare_result: lost_record[i].image_compare_result
+						}
+						matched_lost_record_array.push(lost_data_object);
+						console.log(i+" : push to match");
+					}
+				}
+				var data = {
+					data:matched_lost_record_array,
+					image_compare: "matched"
+				}	
+				console.log("圖片搜尋且有比對到");
+				console.log(data);
+				res.json(data);	
+			}else{
+				var picture_s3_url = "https://d2h10qrqll8k7g.cloudfront.net/person_project/lost_pet/";
+				var unmatch_lost_record_array = [];
+				for(let i=0; i<lost_record.length; i++){
+					var lost_data_object = {
+						id: lost_record[i].pet_id,
+						name: lost_record[i].pet_name,
+						picture: picture_s3_url+lost_record[i].pet_picture,
+						gender: lost_record[i].gender,
+						age: lost_record[i].age,
+						breed: lost_record[i].breed,
+						color: lost_record[i].color,
+						lost_location: lost_record[i].lost_location,
+						lost_location_lng: lost_record[i].lost_location_lng,
+						lost_location_lat: lost_record[i].lost_location_lat,
+						lost_time: lost_record[i].lost_time,
+						lost_status: lost_record[i].lost_status,
+						post_type: lost_record[i].post_type,
+						title: lost_record[i].title,
+						content: lost_record[i].content,
+						image_compare_result: lost_record[i].image_compare_result
+					}
+					unmatch_lost_record_array.push(lost_data_object);
+					console.log(i+" : push to unmatch");
+				};
+				var data = {
+					data:unmatch_lost_record_array,
+					image_compare: "no_matched"
+				}
+				console.log("圖片搜尋沒有比對到");
+				console.log(data);
+				res.json(data);					
+			}		
+		})	
+	}else{
+		console.log("search without file");
+		var picture_s3_url = "https://d2h10qrqll8k7g.cloudfront.net/person_project/lost_pet/";
+		let lost_record_array = [];
+		for(let i=0; i<lost_record.length; i++){
+			var lost_data_object = {
+				id: lost_record[i].pet_id,
+				name: lost_record[i].pet_name,
+				picture: picture_s3_url+lost_record[i].pet_picture,
+				gender: lost_record[i].gender,
+				age: lost_record[i].age,
+				breed: lost_record[i].breed,
+				color: lost_record[i].color,
+				lost_location: lost_record[i].lost_location,
+				lost_location_lng: lost_record[i].lost_location_lng,
+				lost_location_lat: lost_record[i].lost_location_lat,
+				lost_time: lost_record[i].lost_time,
+				lost_status: lost_record[i].lost_status,
+				post_type: lost_record[i].post_type,
+				title: lost_record[i].title,
+				content: lost_record[i].content,
+				image_compare_result: "no_upload"
+			}
+			lost_record_array.push(lost_data_object);
+			console.log(i+" : push without match");
+		};
+		var data = {
+			data:lost_record_array,
+			image_compare: "no_compare"
+		}
+		console.log("沒有使用圖片搜尋");
+		console.log(data);
+		res.json(data);		
+	}
+});
+	
+module.exports = app;
